6049be9f85f053f78e40a396211ac0ca7c238f43
Adrian Gusberti <adrian.m.gusberti@gmail.com>
2020-06-22 02:53:05 -0300

    feat(Conta): Função de transferência

--- src/Conta.php
+++ src/Conta.php
@@ -1,7 +1,7 @@
 <?php
 namespace Classes;
 
-abstract class Conta
+class Conta
 {
   public $agencia;
   public $numero;
@@ -28,6 +28,27 @@ abstract class Conta
       echo "Depósito não realizado! O valor mínimo de depósito é B$ 1,00";
     }
   }
-  
+
+  public function transferencia($valor, $contaDestino) {
+    if($this->saldo > 0 && $this->saldo > $valor) {
+      $this->saldo -= $valor;
+      if($contaDestino != $this->numero) {
+        $numContaDestino = $contaDestino;
+        // Como o sistema não têm integração com BD, esse script cria uma conta de destino temporária com o número e os valor de transferência enviado.
+        $contaDestino = new Conta(100, $numContaDestino, $valor);
+        echo "Transferência de B$ {$valor} realizada com sucesso para a conta de número {$numContaDestino}! <br />";
+
+        // Exibe o saldo atual da conta de destino (em um caso real esse script pode ser desconsiderado) 
+        echo "Dados da conta de destino:";
+        echo $contaDestino->getDetalhes();
+      }
+      else {
+        echo "Transferência não realizada! O número das contas não podem ser iguais. <br />";
+      }
+    }
+    else {
+      echo "Transferência não realizada! Saldo insuficiente. <br />";
+    }
+  }
 }
 ?>
